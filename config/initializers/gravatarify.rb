181c83a35cbf52215fd6c71e5453881f0d60d886
Matt Watson <matt.watson@orasi.com>
2017-08-24 12:49:26 -0400

    update gravatar

---
+++ config/initializers/gravatarify.rb
@@ -0,0 +1,2 @@
+Gravatarify.styles[:default] = { default: :identicon, :html => { :class => 'img-circle' } }
+Gravatarify.styles[:mini] = { size: 45, default: :identicon, :html => { :class => 'img-circle' } }
